a01ef251b1ca408d90b531730a8e002c6281c032
dmigwi <migwindungu0@gmail.com>
2023-07-29 01:41:38 +0300

    Add more bond contract changes

--- contract/bond.sol
+++ contract/bond.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 /// @author dmigwi: (migwindungu0@gmail.com)
 /// @title Bond Contract.
 contract BondContract {
+
     // StatusChoice describes the various stages a bond must pass through.
     // Negotiating => Open to the public, potential bond holder can their
     //                counter proposal hoping to be selected.
@@ -16,11 +17,11 @@ contract BondContract {
     // ContractSigned => Once the final version of the terms are agreed, the final
     //                 document is hashed with the holders keys.
     // BondReselling => The bond holder can re-advertise his bond to anyone else.
-    // Complete   => The issuer has fulfilled his obligation to pay all the amount
+    // BondFinalised => The issuer has fulfilled his obligation to pay all the amount
     //              in full as agreed on in the terms
     enum StatusChoice { 
             Negotiating, HolderSelection, TermsAgreement,
-            ContractSigned, BondReselling, Complete 
+            ContractSigned, BondReselling, BondFinalised 
         }
 
     // CurrencyType defines the various types of currency types supported in the
@@ -44,10 +45,10 @@ contract BondContract {
         // Section: 2 (body)     
         // issuer describes the address of the bond issuer. It should always be
         // set in all bonds.
-        address issuer;
+        address payable issuer;
         // holder describes the address of the current bond holder. Once the
         // bond is resold, the new holder address should be set here.
-        address holder;
+        address payable holder;
         // StatusChoice describes the stages between issuance and completion of
         // a bond.
         StatusChoice status;
@@ -59,10 +60,10 @@ contract BondContract {
         uint8 couponRate;
         // couponDate decribes the interval i.e. monthly, quartely, yearly etc
         // when the interest is due to be paid to the holder.
-        uint8 couponDate;
+        uint32 couponDate;
         // maturityDate decribes date when the whole amount owed to the holder
         // should be cleared/paid in full.
-        uint8 maturityDate;
+        uint32 maturityDate;
         // CurrencyType describes the currency which the bond issue and holder
         // wishes to transact in.
         CurrencyType currency;
@@ -80,4 +81,61 @@ contract BondContract {
         string appendix;
     }
 
+    Bond public bond;
+
+    // Once created the issuer address cannot be changed.
+    constructor() {
+        bond.issuer = payable(msg.sender);
+        bond.status = StatusChoice.Negotiating;
+    }
+
+    // onlyIssuerAllowed restricts bond changes edits to only the issuer.
+    modifier onlyIssuerAllowed {
+       require (
+            msg.sender == bond.issuer,
+            "Only the bond issue can make changes"
+        );
+        _;
+    }
+
+    // setBodyInfo updates the body information. Only the bond issuer can make
+    // this change. 
+    function setBodyInfo(
+        CurrencyType _currency, uint32 _principal, uint8 _couponRate,
+        uint32 _couponDate, uint32 _maturityDate) public onlyIssuerAllowed {
+        bond.currency = _currency;
+        bond.principal = _principal;
+        bond.couponRate = _couponRate;
+        bond.couponDate = _couponDate;
+        bond.maturityDate = _maturityDate;
+    }
+
+    // setStatus sets the bond status. Can be triggered by both the issuer and the
+    // the holder.
+    function setStatus(StatusChoice _status) public {
+        bond.status = _status;
+    }
+
+    // setBondHolder sets the bond holder after negotiations are complete.
+    function setBondHolder(address payable _holder) public {
+        require(msg.sender != _holder, "Bond issuer cannot be the holder too");
+        require(bond.status == StatusChoice.Negotiating, "Cannot set holder during negotiations");
+
+        bond.holder = _holder;
+    }
+
+    // setIntro sets the introduction description. Only the issuer can do this.
+    function setIntro(string memory _intro) public onlyIssuerAllowed {
+        bond.intro = _intro;
+    }
+
+    // setSecurity sets the security information. Only the issuer can do this. 
+    function setSecurity(string memory _security) public onlyIssuerAllowed {
+        bond.security = _security;
+    }
+
+    // setAppendix sets the Appendix information. Only the issuer can do this.
+    function setAppendix(string memory _appendix) public onlyIssuerAllowed {
+        bond.appendix = _appendix;
+    }
 }
