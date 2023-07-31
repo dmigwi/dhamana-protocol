f7c595f5fa55ffc750d900edbbf0e6de7983afb2
dmigwi <migwindungu0@gmail.com>
2023-07-31 22:32:55 +0300

    Add sapphire deployment configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const sapphire = require("@oasisprotocol/sapphire-paratime");
+
 module.exports = {
     contracts_directory: "./contracts",
     migrations_directory: "./contracts/migrations",
@@ -6,6 +9,36 @@ module.exports = {
         host: "127.0.0.1",
         port: 8545,
         network_id: "*"
+      },
+      sapphire_mainnet: {
+        provider: () =>
+          sapphire.wrap(
+            new HDWalletProvider(
+              [process.env.PRIVATE_KEY],
+              "https://sapphire.oasis.io"
+            )
+          ),
+        network_id: 0x5afe,
+      },
+      sapphire_testnet: {
+        provider: () =>
+          sapphire.wrap(
+            new HDWalletProvider(
+              [process.env.PRIVATE_KEY],
+              "https://testnet.sapphire.oasis.dev"
+            )
+          ),
+        network_id: 0x5aff,
+      },
+      sapphire_localnet: {
+        provider: () =>
+          sapphire.wrap(
+            new HDWalletProvider(
+              [process.env.PRIVATE_KEY],
+              "http://localhost:8545"
+            )
+          ),
+        network_id: 0x5afd,
       }
     },
     compilers: {
