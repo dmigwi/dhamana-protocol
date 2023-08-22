2233ca65847ed5431050e31c38541028c046458d
dmigwi <migwindungu0@gmail.com>
2023-08-22 19:03:17 +0300

    prevent committing of devnet deployment config

--- client/deployment/sapphirelocalnet/deployment.go
+++ client/deployment/sapphirelocalnet/deployment.go
@@ -8,40 +8,40 @@ package sapphirelocalnet
 
 // GetNetwork returns the network used to make the deployment.
 func GetNetwork() (string) {
-	return "sapphire_localnet"
+	return ""
 }
 
 // GetBlockGasLimit returns the block gas limit set during deployment.
 func GetBlockGasLimit() (string) {
-	return "0xe4e1c0"
+	return ""
 }
 
 // GetChainID returns the chain ID of the network used to make the deployment.
 func GetChainID() (uint64) {
-	return 23293
+	return 0
 }
 
 // GetContractName returns the name of the contract deployed.
 func GetContractName() (string) {
-	return "ChatContract"
+	return ""
 }
 
 // GetContractAddress returns the address of the deployed contract.
 func GetContractAddress() (string) {
-	return "0x48C85A77897508D5c8976EFa56EeaA93e4226bE5"
+	return ""
 }
 
 // IsDeployed confirms if the contract was successfully deployed. If yes, it succeeded
 func IsDeployed() (bool) {
-	return true
+	return false
 }
 
 // GetTransactionHash returns the tx hash when the contract we deployed.
 func GetTransactionHash() (string) {
-	return "0x93416c146f80f07584eebe1d4c28bac1fa9e3ea7f2634d27be315ae7ce2001dd"
+	return ""
 }
 
 // GetDeploymentTime returns the timestamp in seconds when the contract was actually deployed 
 func GetDeploymentTime() (uint64) {
-	return 1692566525
+	return 0
 }
